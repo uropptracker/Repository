b8f65c4fb22c6cfbb853b4e83fcd222fc2a7efbc
uropptracker <uropptracker@gmail.com>
2014-08-06 22:37:37 +0800

    Add linked list (part 2)

---
+++ ll_insert.cpp
@@ -0,0 +1,24 @@
+#include "ll_cnode.h" /* File: ll_insert.cpp */
+// To insert character c to the linked list so that after insertion,
+// c is the n-th character (counted from zero) in the list.
+// If n > current length, append to the end of the list.
+void ll_insert(ll_cnode*& head, char c, unsigned n)
+{
+    ll_cnode* new_cnode = ll_create(c); // STEP 1: Create the new ll_cnode   
+    if (n == 0 || head == NULL) // Special case: insert at the beginning
+    {
+        new_cnode->next = head;
+        head = new_cnode;
+        return;
+    }
+        
+    // STEP 2: Find the node after which the new node is to be added
+    ll_cnode* p = head; 
+    for (int position = 0; 
+         position < n-1  &&  p->next != NULL; 
+         p = p->next, ++position)
+         ;
+    // STEP 3: Insert the new node between the found node and the next node
+    new_cnode->next = p->next;
+    p->next = new_cnode;
+}
